73fa873f197a56fcf71c53bd9f1476c7349e3e5c
Robert Birkelbach <birkelbach@dzhw.eu>
2019-12-11 14:11:30 +0100

    move copying of env vars to docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -19,6 +19,8 @@ VERSION="${TIMESTAMP}-${TRAVIS_COMMIT}"
 TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${VERSION}"
 aws_access_key_id="${AWS_ACCESS_KEY_ID}"
 aws_secret_access_key="{$AWS_SECRET_ACCESS_KEY}"
+echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
+echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
 # making sure correct region is set
 aws configure set default.region ${EB_REGION} --profile "administrator"
 # Push image to ECR
