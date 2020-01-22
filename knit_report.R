4a2cf6ea0dc210fd64908da5513cc1445686bbef
Robert Birkelbach <birkelbach@dzhw.eu>
2020-01-22 14:49:18 +0100

    add verbosity to s3 requests

--- knit_report.R
+++ knit_report.R
@@ -1,5 +1,20 @@
 library(aws.s3)
+library(here)
+
 source("is_blank.R")
+dir.create(file.path("data-raw"))
+
+setwd(here("data-raw"))
+if (is_blank(Sys.getenv("AWS_EXECUTION_ENV"))) {
+  save_object(object = "data-raw/popular.dta",
+  bucket = "continuous-report-delivery-ffm-public",
+  region = "eu-central-1", ignore_credentials = TRUE)
+} else {
+  print("getting private data")
+  save_object(object = "data-raw/popular.dta",
+    bucket = "continuous-report-delivery-ffm-private", 
+    region = "eu-central-1", verbose = TRUE)
+}
 knitr::knit("report.Rmd", output = "report.html")
 if (is_blank(Sys.getenv("AWS_EXECUTION_ENV"))) {
   file.copy("report.html", "/tmp/report.html")
@@ -7,6 +22,7 @@ if (is_blank(Sys.getenv("AWS_EXECUTION_ENV"))) {
   put_object(file = "./report.html",
     object = "report.html",
     bucket = "continuous-report-delivery-ffm-private",
-    region = "eu-central-1")
+    region = "eu-central-1",
+    verbose = TRUE)
 }
 
