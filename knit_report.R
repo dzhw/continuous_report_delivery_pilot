176133762f492d6b6bbd0ad463dae1961241c493
Robert Birkelbach <birkelbach@dzhw.eu>
2020-01-23 11:08:15 +0100

    try fixing path'

--- knit_report.R
+++ knit_report.R
@@ -19,7 +19,7 @@ rmarkdown::render(here::here("report.Rmd"))
 if (is_blank(Sys.getenv("AWS_EXECUTION_ENV"))) {
   file.copy("report.html", "/tmp/report.html")
 } else {
-  put_object(file = "./report.html",
+  put_object(file = "report.html",
     object = "report.html",
     bucket = "continuous-report-delivery-ffm-private",
     region = "eu-central-1",
