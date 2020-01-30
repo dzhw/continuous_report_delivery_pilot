645e23d9f25ee6a7d954d1f62a309ca94ae46a43
Robert Birkelbach <birkelbach@dzhw.eu>
2020-01-30 10:52:03 +0100

    also generate pdf output

--- knit_report.R
+++ knit_report.R
@@ -16,13 +16,19 @@ if (is_blank(Sys.getenv("AWS_EXECUTION_ENV"))) {
     bucket = "continuous-report-delivery-ffm-private",
     region = "eu-central-1", verbose = TRUE)
 }
-rmarkdown::render(here::here("report.Rmd"))
+rmarkdown::render(here::here("report.Rmd"), output_format = c("html_document", "pdf_document"))
 if (is_blank(Sys.getenv("AWS_EXECUTION_ENV"))) {
   file.copy("report.html", "/tmp/report.html")
+  file.copy("report.pdf", "/tmp/report.pdf")
 } else {
   print(getwd())
   put_object(file = here::here("report.html"),
     object = "report.html",
+    bucket = "continuous-report-delivery-ffm-private",
+    region = "eu-central-1",
+    verbose = TRUE)
+  put_object(file = here::here("report.pdf"),
+    object = "report.pdf",
     bucket = "continuous-report-delivery-ffm-private",
     region = "eu-central-1",
     verbose = TRUE)
